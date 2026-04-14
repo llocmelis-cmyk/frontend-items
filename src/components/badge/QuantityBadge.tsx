ca299747a24a19f0d11c58cf463ba7e3359d6359
lauris <llocmelis@inselo.no>
2026-04-14 09:20:50 +0300

    1.0.2 published

--- src/components/badge/QuantityBadge.tsx
+++ src/components/badge/QuantityBadge.tsx
@@ -12,7 +12,7 @@ export function QuantityBadge({ quantity }: { quantity: number }) {
 
     return (
         <>
-            <span className={`red ml-5 inline-flex items-center rounded-md px-2 py-1 text-lg font-medium  ring-1 ring-inset`}>
+            <span className={`bg-red-50 m-5 inline-flex items-center rounded-md px-5 py-5 text-lg font-medium  ring-1 ring-inset`}>
                 {quantity}
             </span>
         </>
